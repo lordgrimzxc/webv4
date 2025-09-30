8ba8a2c85218c3f0810916b7b5b62f9a382fd71d
lordgrimzxc <joshuadejesus0017@gmail.com>
2025-09-30 10:55:03 +0800

    added my project

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>HELLO WORLD</h1>
+    <v-btn prepend-icon="mdi-account">Submit</v-btn>
+  </div>
+</template>
+
+<script  setup>
+
+</script>
+
+<style>
+
+</style>
+
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/icon',
+  (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+ build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+})
