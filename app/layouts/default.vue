890f1ee82e0e68063b134b14133f61394b18469b
lordgrimzxc <joshuadejesus0017@gmail.com>
2025-10-07 10:23:42 +0800

    Update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,42 +1,66 @@
-<template>
-  <v-card>
-    <v-layout>
-      <v-navigation-drawer
-        expand-on-hover
-        permanent
-        rail
-      >
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
-          ></v-list-item>
-        </v-list>
++<template>
+  <v-app>
+<v-navigation-drawer v-model="drawer" permanent>
+  <v-list>
+    <v-list-item
+      prepend-avatar="https://randomuser.me/api/portraits/men/85.jpeg"
+      subtitle="joshuadejesus017@gmail.com"
+      title="SNC Student"
+    ></v-list-item>
 
-        <v-divider></v-divider>
+  </v-list>
+  <v-divider></v-divider>
+  <v-list density="compact" nav>
 
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+    <v-list-item
+    prepend-icon="mdi-view-dashboard"
+    title="Dashboard"
+    value="myfiles"
+    ></v-list-item>
 
-      <v-main style="height: 250px"></v-main>
-    </v-layout>
-  </v-card>
+    <v-list-item
+    prepend-icon="mdi-store"
+    title="Inventory"
+    value="shared"
+    to="/inventory"
+    ></v-list-item>
+
+    <v-list-item
+    prepend-icon="mdi-shape"
+    title="categories"
+    value="starred"
+    to="/categories"
+    ></v-list-item>
+
+    <v-list-item
+    prepend-icon="mdi-truck"
+    title="Suppliers"
+    value="starred"
+    to=""
+    ></v-list-item>
+
+  </v-list>
+
+</v-navigation-drawer>
+      <v-app-bar elevation="1">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+      </v-app-bar>
+
+      <v-main>
+        <v-container fluid>
+          <slot/>
+        </v-container>
+      </v-main>
+
+  </v-app>
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+const drawer = ref(true)
 </script>
 
-<script>
-  export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
-  }
-</script>
+<style>
+</style>
--- app/pages/inventory/index.vue
+++ app/pages/inventory/index.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    
+    <h1>Inventory</h1>
+    <table border="1" cellpadding="20" cellspacing="20">
+      <thead>
+        <tr>
+          <th>Item Name</th>
+          <th>Category</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in inventory" :key="index">
+          <td>{{ item.name }}</td>
+          <td>{{ item.category }}</td>
+          
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+const inventory = [
+  { name: 'Apple', category: 'Fruit',},
   
-}
+];
 </script>
 
 <style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
 
+
+td {
+  padding: 8px;
+}
 </style>
